00c0fadd5dc9c19fe2469742aa15ef4c1cf447f7
VayuDev <vayudev@protonmail.com>
2020-05-16 21:38:46 +0200

    Frontend: Fixed bugs with multiple message boxes

--- html/js/index.js
+++ html/js/index.js
@@ -1,17 +1,20 @@
 let lowestBoxHeight = 200;
 let lowestBoxId = -1;
 let boxIdCounter = 0;
+let boxCount = 0;
 
 function notifyError(msg) {
     notify(msg, "notify-error");
 }
 
 function notify(msg, classname = "notify-info") {
+    let msgBox = $("<div></div>").text(msg).addClass(classname).addClass("notify")
+    $("body").append(msgBox);
     let boxHeight = lowestBoxHeight;
-    let msgBox = $("<div></div>").text(msg).addClass(classname).addClass("notify");
     msgBox.css("top", boxHeight + "px");
-    let id = boxIdCounter++;
+    let id = ++boxIdCounter;
     lowestBoxId = id;
+    ++boxCount;
 
     let animationCounter = 0;
     let intervalID = setInterval(function() {
@@ -19,10 +22,9 @@ function notify(msg, classname = "notify-info") {
         if(animationCounter > 70) {
             boxHeight -= 2.5;
             msgBox.css("top", boxHeight + "px");
-            if(lowestBoxId === id) {
-                lowestBoxHeight = Math.max(boxHeight, 200);
-            }
-
+        }
+        if(lowestBoxId === id) {
+            lowestBoxHeight = boxHeight + msgBox.height() + 20;
         }
         if(animationCounter >= 100) {
             msgBox.css("opacity", (150 - animationCounter) / 50.0);
@@ -31,9 +33,13 @@ function notify(msg, classname = "notify-info") {
         if(animationCounter >= 150) {
             clearInterval(intervalID);
             msgBox.detach();
+            --boxCount;
+            if(boxCount === 0) {
+                lowestBoxHeight = 200;
+            }
         }
     }, 20);
-    $("body").append(msgBox);
+
 }
 
 $(function() {
