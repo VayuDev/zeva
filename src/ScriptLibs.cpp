7db7569363ae6d5aa34fc485e5f6eed76d374220
VayuDev <vayudev@protonmail.com>
2020-06-03 00:03:38 +0200

    Added native script lib for converting HSV to RGB
    This function is called Image.hsvToRgb(r, g, b).
    
    This greatly increases the speed of converting values from HSV to RGB
    compared to a simple in-script implementation from inside wren. Image
    rendering times have gone in one case from 180ms to 120ms, making this a
    great performance improvement :^).

--- src/ScriptLibs.cpp
+++ src/ScriptLibs.cpp
@@ -221,6 +221,69 @@ static void tcpClientRecvString(WrenVM* pVM) {
     }
 }
 
+static void hsvToRgb(WrenVM* pVM) {
+    if(wrenGetSlotType(pVM, 1) != WREN_TYPE_NUM || wrenGetSlotType(pVM, 2) != WREN_TYPE_NUM || wrenGetSlotType(pVM, 3) != WREN_TYPE_NUM) {
+        wrenSetSlotNull(pVM, 0);
+        LOG_WARN << "ScriptLibs: hsvToRgb invalid parameters";
+        return;
+    }
+    auto h = wrenGetSlotDouble(pVM, 1);
+    auto s = wrenGetSlotDouble(pVM, 2);
+    auto v = wrenGetSlotDouble(pVM, 3);
+    double      p, q, t, ff;
+    long        i;
+    double r, g, b;
+    while(h >= 360.0) h -= 360.0;
+    while(h < 0) h += 360.0;
+    h /= 60.0;
+    i = (long)h;
+    ff = h - i;
+    p = v * (1.0 - s);
+    q = v * (1.0 - (s * ff));
+    t = v * (1.0 - (s * (1.0 - ff)));
+
+    switch(i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+        case 5:
+        default:
+            r = v;
+            g = p;
+            b = q;
+            break;
+    }
+    wrenSetSlotNewList(pVM, 0);
+    wrenSetSlotDouble(pVM, 1, r*255);
+    wrenInsertInList(pVM, 0, 0, 1);
+    wrenSetSlotDouble(pVM, 1, g*255);
+    wrenInsertInList(pVM, 0, 1, 1);
+    wrenSetSlotDouble(pVM, 1, b*255);
+    wrenInsertInList(pVM, 0, 2, 1);
+}
+
 static void exportImage(WrenVM* pVM) {
     if(wrenGetSlotType(pVM, 1) != WREN_TYPE_LIST || wrenGetSlotType(pVM, 2) != WREN_TYPE_NUM || wrenGetSlotType(pVM, 3) != WREN_TYPE_NUM) {
         wrenSetSlotNull(pVM, 0);
@@ -295,6 +358,10 @@ WrenForeignMethodFn bindForeignMethod(
         if(strcmp("exportInternal(_,_,_)", signature) == 0) {
             return exportImage;
         }
+    } else if(strcmp(className, "Image") == 0 && isStatic) {
+        if(strcmp("hsvToRgb(_,_,_)", signature) == 0) {
+            return hsvToRgb;
+        }
     } else if(strcmp(className, "Math") == 0 && isStatic) {
         if(strcmp("random()", signature) == 0) {
             return genRandomDouble;
@@ -336,6 +403,11 @@ class Image {
     foreign exportInternal(pixels, width, height)
     export() {
         return exportInternal(_pixels, _width, _height)
+    }
+
+    foreign static hsvToRgb(h, s, v)
+    static hsvToRgb(color) {
+        return hsvToRgb(color[0], color[1], color[2])
     }
 }
 
