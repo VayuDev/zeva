c59778569e9a0391a3fae4908793462946805633
VayuDev <vayudev@protonmail.com>
2020-06-02 10:20:27 +0200

    Added continous streaming support to script image renderer

--- html/scripts/js/draw.js
+++ html/scripts/js/draw.js
@@ -1,6 +1,22 @@
 const params = new URLSearchParams(window.location.search);
 const SCRIPTID = Number(params.get("scriptid"));
 
+function loadImage() {
+    $("#output").attr("src", "/api/scripts/draw?scriptid=" + SCRIPTID + "&seed=" + Math.random());
+}
+
+function checkCheckbox() {
+    if($("#streambox").is(":checked")) {
+        console.log("Streaming")
+        $("#output").on("load", loadImage);
+        loadImage();
+    } else {
+        $("#output").off("load");
+    }
+}
+
 $(function() {
-    $("#output").attr("src", "/api/scripts/draw?scriptid=" + SCRIPTID);
+    loadImage();
+    $("#streambox").click(checkCheckbox);
+    checkCheckbox();
 })
