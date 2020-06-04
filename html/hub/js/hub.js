348d7acb4e7c0b6c253a4763aada735895d20dba
VayuDev <vayudev@protonmail.com>
2020-06-04 14:06:57 +0200

    Added searchbar to the hub

--- html/hub/js/hub.js
+++ html/hub/js/hub.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    $("#searchbar").keypress((key) => {
+        if(key.which === 13)
+            window.location = "https://google.com/search?q=" + $("#searchbar").val();
+    })
 })
