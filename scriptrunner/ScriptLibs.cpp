1557f3b9cb51ee87c4d26a9bae170f445b9cbc48
VayuDev <vayudev@protonmail.com>
2020-06-22 23:03:49 +0200

    Sketch out notification sender api in wren

--- scriptrunner/ScriptLibs.cpp
+++ scriptrunner/ScriptLibs.cpp
@@ -520,6 +520,61 @@ foreign class TcpClient {
     foreign recvString()
     foreign close()
 }
+/*
+class HttpResponse {
+    construct new(type, body) {
+        _type = type
+        _body = body
+    }
+    construct new(error) {
+        _error = error
+    }
+    error { _error }
+    type { _type }
+    body { _body }
+    toString {
+        if(_error == null) {
+            return "HttpResponse{ type=%(_type), body=%(_body) }"
+        } else {
+            return "HttpResponse{ error=%(_error) }"
+        }
+    }
+}
+
+foreign class HttpClient {
+    foreign construct new(hostname)
+    foreign sendInternal(type, url, body)
+    send(type, url, body) {
+        var resp = sendInternal(type, url, body)
+        var type = resp[0]
+        if(type == "ok") {
+            var body = resp[1]
+            return HttpResponse.new(type, body)
+        } else {
+            return HttpResponse.new(type)
+        }
+    }
+}
+
+class Config {
+    static foreign getConfigStringInternal()
+    static get() {
+        if(__json == null) {
+            __json = JSON.parse(getConfigStringInternal())
+        }
+        return __json
+    }
+}
+
+class NotificationClient {
+    construct new(type) {
+        if(type != "gotify") {
+            throw "Unknown type: %(type)"
+        }
+        var config = Config.get()["notification"]["gotify"]
+        _connection = HttpClient.new(config["hostname"], config["port"])
+    }
+}*/
 
 class Image {
     construct new(width, height) {
