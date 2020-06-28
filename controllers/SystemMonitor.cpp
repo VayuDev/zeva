33b69df54577cfc5ec6650b9e744898412a81d81
VayuDev <vayudev@protonmail.com>
2020-06-28 13:47:11 +0200

    Added system_monitor addRow backend

--- controllers/SystemMonitor.cpp
+++ controllers/SystemMonitor.cpp
@@ -29,3 +29,30 @@ void Api::Db::SystemMonitor::getConfig(
       },
       genErrorHandler(callback), deviceName);
 }
+void Api::Db::SystemMonitor::addRow(
+    const drogon::HttpRequestPtr &req,
+    std::function<void(const drogon::HttpResponsePtr &)> &&callback,
+    std::string &&data) {
+  Json::Value receivedJson;
+  std::stringstream receivedStream{data};
+  receivedStream >> receivedJson;
+  drogon::app().getDbClient()->execSqlAsync(
+      "INSERT INTO system_monitor_entry (deviceid, time, total_cpu_usage, "
+      "cpu_core_count, uptime, load_1, load_5, load_15, total_ram, free_ram, "
+      "shared_ram, buffer_ram, total_swap, free_swap, proc_count, total_high, "
+      "free_high) VALUES ($1, current_timestamp, $2, $3, $4, $5, $6, $7, $8, "
+      "$9, $10, $11, "
+      "$12, $13, $14, $15, $16)",
+      [callback](const drogon::orm::Result &res) mutable {
+
+      },
+      genErrorHandler(callback), receivedJson["machine_id"].asInt64(),
+      receivedJson["total_cpu_usage"].asDouble(),
+      receivedJson["cpu_core_count"].asInt(), receivedJson["uptime"].asInt64(),
+      receivedJson["load_1"].asDouble(), receivedJson["load_5"].asDouble(),
+      receivedJson["load_15"].asDouble(), receivedJson["total_ram"].asInt(),
+      receivedJson["free_ram"].asInt(), receivedJson["shared_ram"].asInt(),
+      receivedJson["buffer_ram"].asInt(), receivedJson["total_swap"].asInt(),
+      receivedJson["free_swap"].asInt(), receivedJson["proc_count"].asInt(),
+      receivedJson["total_high"].asInt(), receivedJson["free_high"].asInt());
+}
--- controllers/SystemMonitor.hpp
+++ controllers/SystemMonitor.hpp
@@ -9,12 +9,16 @@ class SystemMonitor : public drogon::HttpController<SystemMonitor> {
 public:
   METHOD_LIST_BEGIN
   METHOD_ADD(SystemMonitor::getConfig, "config?devicename={}", drogon::Get);
+  METHOD_ADD(SystemMonitor::addRow, "addRow?data={}", drogon::Post);
   METHOD_LIST_END
 
   void
   getConfig(const drogon::HttpRequestPtr &req,
             std::function<void(const drogon::HttpResponsePtr &)> &&callback,
             std::string &&deviceName);
+  void addRow(const drogon::HttpRequestPtr &req,
+              std::function<void(const drogon::HttpResponsePtr &)> &&callback,
+              std::string &&data);
 };
 
 } // namespace Api::Db
