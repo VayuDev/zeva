3a371846b0cb852bc8a1e195b677c4f4c6f9668e
VayuDev <vayudev@protonmail.com>
2020-07-01 21:22:14 +0200

    Add buttons for switching hub search engine

--- html/hub/js/hub.js
+++ html/hub/js/hub.js
@@ -1,6 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     $("#searchbar").keypress((key) => {
-        if(key.which === 13)
-            window.location = "https://google.com/search?q=" + $("#searchbar").val();
+        if(key.which === 13) {
+            window.location = ENGINES[active] + $("#searchbar").val();
+        }
     })
 })
+
+let active = localStorage.getItem("engine");
+if(!active) {
+    active = "ddg";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    selectEngine(document.querySelector("[engine=" + active + "]"));
+    setTimeout(() => {
+        $(".logo").css("transition", "filter 0.5s");
+    }, 500)
+
+});
+
+const ENGINES = {
+    "google": "https://google.com/search?q=",
+    "ddg": "https://duckduckgo.com/?q=",
+    "qwant": "https://www.qwant.com/?q="
+}
+
+function selectEngine(engineElement) {
+    $(".logo").addClass("inactive");
+    $(engineElement).removeClass("inactive");
+    active = engineElement.getAttribute("engine");
+    localStorage.setItem("engine", active);
+}
