9774584329b964a733db43027a388806c007d044
VayuDev <vayudev@protonmail.com>
2020-07-01 21:51:31 +0200

    Hub: Cache wallpaper for three minutes to increase load times

--- controllers/Hub.cpp
+++ controllers/Hub.cpp
@@ -4,10 +4,23 @@
 #include <filesystem>
 
 void Api::Hub::getRandomWallpaper(
-    const drogon::HttpRequestPtr &,
+    const drogon::HttpRequestPtr &req,
     std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
+  // check how old the client image is
+  const auto& ifModifiedSinceHeader = req->getHeader("if-modified-since");
+  if(!ifModifiedSinceHeader.empty()) {
+    auto imageFrom = drogon::utils::getHttpDate(ifModifiedSinceHeader);
+    if(imageFrom >= trantor::Date::now().after(-180)) {
+      // cached response
+      auto resp = drogon::HttpResponse::newHttpResponse();
+      resp->setStatusCode(drogon::k304NotModified);
+      callback(resp);
+      return;
+    }
+  }
+
   std::filesystem::directory_iterator dir{"html/wallpapers"};
-  srand(time(0));
+  srand(time(nullptr));
   std::vector<std::string> filenames;
   for (const auto &image : dir) {
     if (image.is_regular_file()) {
@@ -16,10 +29,13 @@ void Api::Hub::getRandomWallpaper(
   }
   if (filenames.empty()) {
     callback(genError("No wallpapers!"));
+    return;
   }
+
   auto file = filenames.at(rand() % filenames.size());
   auto resp = drogon::HttpResponse::newFileResponse(file);
-  resp->addHeader("Cache-Control",
-                  "max-age=0, no-cache, must-revalidate, proxy-revalidate");
+  //resp->addHeader("expires",drogon::utils::getHttpFullDate(trantor::Date::now().after(180000)));
+  resp->addHeader("last-modified",drogon::utils::getHttpFullDate(trantor::Date::now()));
+  resp->addHeader("cache-control", "public, max-age=604800, immutable");
   callback(resp);
 }
